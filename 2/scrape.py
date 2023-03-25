e8d3bff390ee0f92ea87502560648dc7e3204588
gotgotaind <gotgotaind@gmail.com>
2023-03-25 01:00:50 +0100

    Fixed selenium/chromium failure on some setup

--- 2/scrape.py
+++ 2/scrape.py
@@ -1,11 +1,21 @@
 
 
-from selenium import webdriver
+#from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
-#driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-driver = webdriver.Chrome()
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('/usr/bin/chromedriver',options=chrome_options)
+
+
+#driver = webdriver.Chrome("/usr/bin/chromedriver")
+#driver = webdriver.Chrome()
 
 products=[] #List to store name of the product
 prices=[] #List to store price of the product
@@ -13,6 +23,7 @@ ratings=[] #List to store rating of the product
 driver.get("https://nominis.cef.fr/contenus/prenom/alphabetique/B.html")
 
 content = driver.page_source
+print(content)
 soup = BeautifulSoup(content)
 for a in soup.findAll('a',href=True, attrs={'class':'_31qSD5'}):
     name=a.find('div', attrs={'class':'_3wU53n'})
