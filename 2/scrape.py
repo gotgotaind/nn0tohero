9bff3c7a388004d0e2c9a92016853db7f6b0767c
root <root@zozo-PC.localdomain>
2023-03-26 23:38:17 +0200

    Scraped a firstname list...

--- 2/scrape.py
+++ 2/scrape.py
@@ -1,6 +1,7 @@
-
-
 #from selenium import webdriver
+import string
+import time
+
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -13,19 +14,38 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('/usr/bin/chromedriver',options=chrome_options)
 
+output="prenoms.txt"
 
+print(string.ascii_uppercase)
 #driver = webdriver.Chrome("/usr/bin/chromedriver")
 #driver = webdriver.Chrome()
 
 products=[] #List to store name of the product
 prices=[] #List to store price of the product
 ratings=[] #List to store rating of the product
-driver.get("https://nominis.cef.fr/contenus/prenom/alphabetique/B.html")
+for letter in string.ascii_uppercase:
+    page=0
+    stop=False
+    with open(output, "a") as f:
+        while( not stop ):
+            if( page == 0 ):
+                suffix=''
+            else:
+                suffix="-"+str(page)
+            url="https://nominis.cef.fr/contenus/prenom/alphabetique/"+letter+suffix+".html"
+            print(f'scraping {url}')
+            driver.get(url)
 
-content = driver.page_source
-#print(content)
-soup = BeautifulSoup(content)
-#for a in soup.findAll( attrs={'class':'mb-1'}):
-for a in soup.findAll( class_="mb-1"):
-    print(a.string)
-    #name=a.find('div', attrs={'class':'_3wU53n'})
+            content = driver.page_source
+            #print(content)
+            soup = BeautifulSoup(content,features="html.parser")
+            #for a in soup.findAll( attrs={'class':'mb-1'}):
+            found=0
+            for a in soup.findAll( class_="mb-1"):
+                print(a.string,file=f)
+                found=found+1
+                #name=a.find('div', attrs={'class':'_3wU53n'})
+            if( found == 0 ):
+                stop=True
+            time.sleep(1)
+            page=page+1
