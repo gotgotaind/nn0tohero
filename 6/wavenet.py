0319eef92b7867059a91a38b28ebd9a25a534bed
root <root@zozo-PC.localdomain>
2023-05-14 00:15:57 +0200

    6

---
+++ 6/wavenet.py
@@ -0,0 +1,6 @@
+# https://www.youtube.com/watch?v=t3YJ5hKiMQ0&list=PLAqhIrjkxbuWI23v9cThsA9GvCAUhRvKZ&index=6
+# https://github.com/karpathy/makemore
+# https://github.com/karpathy/nn-zero-to-hero/blob/master/lectures/makemore/makemore_part5_cnn1.ipynb
+# https://colab.research.google.com/drive/1CXVEmCO_7r7WYZGb5qnjfyxTvQa13g5X?usp=sharing
+# https://discord.com/invite/3zy8kqD9Cp
+# https://arxiv.org/abs/1609.03499
